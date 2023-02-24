34c5129bc9d767010c30f811cdb92c6b3ab5e6ba
AdrianMay200 <adrian.19070041@itsmotul.edu.mx>
2023-02-24 15:09:15 -0600

    Actualizado

---
+++ Prototipo/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Personajes from './components/Personajes.vue'
+
+</script>
+
+<template>
+
+ 
+
+  <header>
+
+    <div>
+
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink> 
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/personajes">Personajes</RouterLink>
+      </nav>
+    </div>
+  </header>
+  
+
+  <RouterView />
+</template>
+
+
